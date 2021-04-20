8fdbc813536c6379a7febeebae5a5c16a924d657
Vini Fraga <vinifragam@gmail.com>
2021-04-20 02:09:19 -0300

    Aula 02

--- nlw5.0/reactnative/plantmanager/src/components/Button.tsx
+++ nlw5.0/reactnative/plantmanager/src/components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
@@ -8,31 +9,28 @@ interface ButtonProps extends TouchableOpacityProps {
 
 export function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity
-      style={styles.button}
-      activeOpacity={0.7}
+    <TouchableOpacity 
+      style={styles.container} 
       {...rest}
-      >
-        <Text style={styles.buttonText}>
-          { title }
-        </Text>
-      </TouchableOpacity>
+    >
+      <Text style={styles.text}>
+        { title }
+      </Text>
+    </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
     height: 56,
-    width: 56,
-    paddingHorizontal: 10
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  buttonText: {
+  text: {
+    fontSize: 16,
     color: colors.white,
-    fontSize: 24
+    fontFamily: fonts.heading
   }
-});
+})

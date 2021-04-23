6e578253ad3d447959b3df3a2659427ad5897129
Vini Fraga <vinifragam@gmail.com>
2021-04-23 17:20:43 -0300

    fix android notifications

--- nlw5.0/reactnative/plantmanager/App.tsx
+++ nlw5.0/reactnative/plantmanager/App.tsx
@@ -17,6 +17,14 @@ export default function App() {
   });
 
   useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
     const subscription = Notifications.addNotificationReceivedListener(
       async notification => {
         const data = notification.request.content.data.plant as PlantProps;

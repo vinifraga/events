cf8145083317f5fb749f8910da3d63a789ee21b8
Vini Fraga <vinifragam@gmail.com>
2021-06-17 00:55:06 -0300

    Aula 01

---
+++ nlw_together/gameplay/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  return (
+    <SignIn />
+  )
+}
---
+++ nlw_together/gameplay/src/screens/SignIn/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, Image, View, StatusBar } from 'react-native';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import { styles } from './styles';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {'\n'}
+          suas jogatinas {'\n'}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seus amigos
+        </Text>
+        
+        <ButtonIcon 
+          title="Entrar com Discord" 
+          activeOpacity={0.7} 
+        />
+      </View>
+    </View>
+  )
+}

bbeaba1f08270895240d2827553019ff819db62a
Vini Fraga <vinifragam@gmail.com>
2022-12-08 16:08:52 -0300

    Aula 01 - Fundamentos do Nest.js & Prisma

--- ignite-lab-nodejs-1/notifications-service/src/app.module.ts
+++ ignite-lab-nodejs-1/notifications-service/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { HTTPModule } from './http.module';
+
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [HTTPModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
